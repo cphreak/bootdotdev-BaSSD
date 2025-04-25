a82eff036c8ff898190d8b667f18a4704611987e
scl <6153394+cphreak@users.noreply.github.com>
2025-04-24 22:40:08 -0700

    touch

--- src/test_main.py
+++ src/test_main.py
@@ -145,7 +145,7 @@ class TestText2HTMLNode(unittest.TestCase):
 
 # text_to_textnodes
 # how to keep in order?
-# Just cheating again
+# Just cheating again and again
     def test_text_to_textnodes(self):
         text = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
         new_nodes = text_to_textnodes(text)
